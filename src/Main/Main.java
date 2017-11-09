37955cd8f0b9cc74ed40ced073cf0c72c3da3a1f
GuerraUTFPR <guerramatheus2@gmail.com>
2017-11-08 22:43:15 -0200

    Classe de acesso ao token criada

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,17 +5,32 @@
  */
 package Main;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.FacebookClient.AccessToken;
+import com.restfb.Parameter;
+import com.restfb.types.Page;
+import com.restfb.types.User;
+
 /**
  *
  * @author guerra
  */
 public class Main {
-
+    final String appID = "1679649692066290";
+    final String appSecret = "37f5ed9d9357dff29314217f82fc3228";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AccessToken at = new AccessToken();
+        
+        User user = facebookClient.fetchObject("me", User.class);
+        Page page = facebookClient.fetchObject("cocacola", Page.class,
+                Parameter.with("fields", "fan_count"));
+
+        out.println("User name: " + user.getName());
+        out.println("Page likes: " + page.getFanCount());
     }
-    
+
 }
